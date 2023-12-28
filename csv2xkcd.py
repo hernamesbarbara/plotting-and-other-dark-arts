d600fcc49fd482892d48ed3ff6ce73fc620bc6da
austin <tips@cia.lol>
2023-12-28 18:34:42 -0500

    plots

--- csv2xkcd.py
+++ csv2xkcd.py
@@ -1,50 +1,66 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 
+matplotlib.rcParams['font.family'] = 'Humor Sans'
+
+
+def smooth_series(series, window_length=5, poly_order=2):
+    return savgol_filter(series, window_length, poly_order)
+
+
+def plot_smoothed_data(df, xlabel, ylabel, title, subtitle, xticks_step=2, linewidth=3.5, xlabel_fontsize=18, ylabel_fontsize=18, title_fontsize=24, plot_size=8):
+    # Apply XKCD style
+    plt.xkcd(scale=2.5, length=250, randomness=10)
+    fig, ax = plt.subplots(figsize=(plot_size, plot_size//1.15))
+
+    # Plot
+    ax.plot(df['date'], df['n_smooth'], color='#00b42e',
+            linewidth=linewidth, solid_capstyle='round')
+
+    ax.set_xlim(left=0)
+    ax.set_ylim(bottom=0)
+
+    # X-axis formatting
+    # Adjust tick step for readability
+    ax.set_xticks(df['date'][::xticks_step])
+    ax.set_xticklabels(df['date_label'][::xticks_step], rotation=45)
+    ax.set_xlabel(xlabel, fontsize=xlabel_fontsize)
+
+    # Y-axis formatting
+    ax.set_ylabel(ylabel, fontsize=ylabel_fontsize)
+    ax.tick_params(axis='y', labelsize=0)
+
+    # Adjust y-ticks number
+    current_yticks = ax.get_yticks()
+    new_yticks = np.linspace(current_yticks[0], current_yticks[-1], 10)
+    ax.set_yticks(new_yticks)
+
+    # Adjust spines
+    ax.spines['top'].set_linewidth(0)  # Hide top spine
+    ax.spines['right'].set_linewidth(0)  # Hide right spine
+    ax.spines['bottom'].set_linewidth(linewidth)
+    ax.spines['left'].set_linewidth(linewidth)
+
+    # Grid and Title
+    ax.grid(False)
+    ax.set_title(title, fontsize=title_fontsize)
+    ax.text(0.5, 1.03, subtitle, transform=ax.transAxes,
+            ha="center", va="top", fontsize=title_fontsize*0.75)
+
+    plt.tight_layout()
+    plt.show()
+
+
+# Load data
 csv_file = "data.csv"
-
 df = pd.read_csv(csv_file, sep=',')
+df['n_smooth'] = smooth_series(df['n'])
+# df.loc[0, 'n_smooth'] = 0
 
-window_length = 5
-poly_order = 2
-df['n_smooth'] = savgol_filter(df['n'], window_length, poly_order)
-
-# Plotting adjustments
-plt.xkcd()  # Apply XKCD style
-fig, ax = plt.subplots()
-
-ax.plot(df['date'], df['n_smooth'], color='#00b42e',
-        linewidth=4, solid_capstyle='round')
-
-# Setting x-axis ticks and labels
-ax.set_xticks(df['date'][::2])  # only label every-other date for readability
-# rorate x labs 45 degrees too
-ax.set_xticklabels(df['date label'][::2], rotation=45)
-
-ax.set_ylabel("i'm flying !!", fontsize=24)  # 20pt increased by 20%
-
-# rm y-tick labs
-ax.tick_params(axis='y', labelsize=0)
-
-# specifiy num y-ticks
-current_yticks = ax.get_yticks()
-new_yticks = np.linspace(current_yticks[0], current_yticks[-1], 10)
-ax.set_yticks(new_yticks)
-
-ax.spines['top'].set_linewidth(0)  # Hide the top spine
-ax.spines['right'].set_linewidth(0)  # Hide the right spine
-ax.spines['bottom'].set_linewidth(4)
-ax.spines['left'].set_linewidth(4)
-
-# Removing gridlines
-ax.grid(False)
-
-ax.set_title("from thoropass.arr import antigravity", fontsize=30)
-
-ax.set_xlabel('')
-
-
-plt.tight_layout()
-plt.show()
+plot_size = 8  # 8x8 inches for a square plot
+plot_smoothed_data(df, xlabel="this is my horse. my horse is amazing.", ylabel="i'm flying !!",
+                   title="from thoropass.arr import antigravity", subtitle='',
+                   plot_size=plot_size)
